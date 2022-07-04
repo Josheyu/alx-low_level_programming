6ad039bd5fe42f0bd700458094b1a1e7461a36a1
Josheyu <eyasu.ermi@gmail.com>
2022-07-04 22:54:01 +0300

    upp

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,10 +3,11 @@
 #include <stdio.h>
 
 /**
- * @brief main -> assign a random number to the variable n each
- * 
- * @return int Return -> Success always 0
- */
+*main -> assign a random number to the variable n each time it is executed
+*based a condition
+*Return: always 0
+*/
+
 int main(void)
 {
 int ch;
