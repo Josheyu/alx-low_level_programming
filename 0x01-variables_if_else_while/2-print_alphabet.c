e5c48c897f2526224203a77d5caf1a87de214db9
Josheyu <eyasu.ermi@gmail.com>
2022-07-04 22:11:58 +0300

    Task 2.

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main -> assign a random number to the variable n each
- * and print the last digit of the stored in the variable
- * Return : always 0
- */
+* main -> assign a random number to the variable n each
+* and print the last digit of the stored in the variable
+* Return : always 0
+*/
 
 int main(void)
 {
