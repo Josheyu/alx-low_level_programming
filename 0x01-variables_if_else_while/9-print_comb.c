5aed455869a03148a65149e2d545e839a3092517
Josheyu <eyasu.ermi@gmail.com>
2022-07-04 23:03:47 +0300

    up9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
-*main -> assign a random number to the variable n each
-*and print the last digit of the stored in the variable
-*Return : always 0
+*main -> assign a random number to the variable n each time it is executed
+*based a condition
+*Return: always 0
 */
+
 int main(void)
 {
 int ch;
