71df5acd88ac751b7c50e6c5ac919ed843c50691
Josheyu <eyasu.ermi@gmail.com>
2022-07-07 22:43:24 +0300

    task323

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,5 +1,50 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "main.h"
+
+int convert_day(int month, int day)
+{
+    switch (month)
+    {
+        case 2:
+            day = 31 + day;
+            break;
+        case 3:
+            day = 59 + day;
+            break;
+        case 4:
+            day = 90 + day;
+            break;
+        case 5:
+            day = 120 + day;
+            break;
+        case 6:
+            day = 151 + day;
+            break;
+        case 7:
+            day = 181 + day;
+            break;
+        case 8:
+            day = 212 + day;
+            break;
+        case 9:
+            day = 243 + day;
+            break;
+        case 10:
+            day = 273 + day;
+            break;
+        case 11:
+            day = 304 + day;
+            break;
+        case 12:
+            day = 334 + day;
+            break;
+        default:
+            break;
+    }
+    return (day);
+}
 
 /**
 * print_remaining_days - takes a date and prints how many days are
@@ -34,4 +79,15 @@ printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 365 - day);
 }
 }
+}
+
+/**
+* main - debugging example
+* Return: 0
+*/
+int main(void)
+{
+    int day = convert_day(04,01);
+print_remaining_days(4,day,1997);
+return (0);
 }
