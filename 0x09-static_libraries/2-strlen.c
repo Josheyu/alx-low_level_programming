296163ae9319bdce3c56ee822ceed4a951af9371
Josheyu <eyasu.ermi@gmail.com>
2022-07-24 03:27:10 -0700

    Task 0 All

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - Function that returns the length of a string
+ *
+ * @s: this is the input string
+ *
+ * Return: Length of the string
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
