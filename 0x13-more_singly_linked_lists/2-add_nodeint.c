e570613bf5764fb76e77f540c142e2870381c6db
Josheyu <eyasu.ermi@gmail.com>
2022-08-16 12:01:02 -0700

    Task 2 Up

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -3,7 +3,7 @@
 /**
  * add_nodeint - adds a node to the beginning of a linked list
  * @head: pointer to the head of the list
- * 2n: integer to be used as content
+ * @n: integer to be used as content
  *
  * Return: address of the newly added node
  */
